7258c8f32f1de9ba5a08c8ed4988edee44929b5a
Abhishek Jain <1709abhishek@gmail.com>
2020-04-24 10:32:57 +0530

    added create-area

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Note from './components/Note'
+import CreateArea from './components/CreateArea'
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <CreateArea />
         <Note title='This is the note title' content='This is the note content' />
         <Footer />
       </div>
